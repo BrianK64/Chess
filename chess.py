44aa97f109e5b2c6bd9d3907387e0f1498854a0b
BrianK64 <76234532+BrianK64@users.noreply.github.com>
2023-04-29 15:37:58 -0400

    Uploaded chess.py
    Completed parts: Chess class and some portions of Player1 and Player2 class.

---
+++ chess.py
@@ -0,0 +1,165 @@
+import random
+import copy
+
+file = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+rank = ['8', '7', '6', '5', '4', '3', '2', '1']
+
+class Chess:
+    
+    def __init__(self):
+        self.board = [[' ' for file in range(8)] for rank in range(8)]
+        self.player1 = ''
+        self.player1_color = ''
+        self.player2 = ''
+        self.player2_color = ''
+
+        # For simplicity
+        self.num_of_rank = 8
+        self.num_of_file = 8
+
+    def get_board(self):
+        for rank in self.board:
+            rank_blocks = []
+            for block in rank:
+                if len(block) == 2:
+                    rank_blocks.append(block[0])
+                else:
+                    rank_blocks.append(block)
+            print(rank_blocks)
+
+    def setup_board(self):
+        # P: Pawn, B: Bishop, N: Knight, R: Rook, Q: Queen, K: King
+        
+        # Initialize Black pieces.
+        self.board[0][0] = self.board[0][-1] = ['R', 'Black']
+        self.board[0][1] = self.board[0][-2] = ['N', 'Black']
+        self.board[0][2] = self.board[0][-3] = ['B', 'Black']
+        self.board[0][3] = ['K', 'Black']
+        self.board[0][4] = ['Q', 'Black']
+
+        self.board[-1][0] = self.board[-1][-1] = ['R', 'White']
+        self.board[-1][1] = self.board[-1][-2] = ['N', 'White']
+        self.board[-1][2] = self.board[-1][-3] = ['B', 'White']
+        self.board[-1][3] = ['K', 'White']
+        self.board[-1][4] = ['Q', 'White']
+
+
+        # Initialize second rank of each side
+        for block in range(self.num_of_file):
+            self.board[1][block] = ['P', 'Black']
+            self.board[-2][block] = ['P', 'White']
+
+    def select_player(self):
+        color = ['White', 'Black']
+        (self.player1_color, self.player2_color) = random.sample(color, 2)
+        print(f'{self.player1} has {self.player1_color} piece and {self.player2} has {self.player2_color} piece')
+
+        if self.player1_color == 'White':
+            return (f'{self.player1} goes first')
+        else:
+            return (f'{self.player2} goes first')
+
+
+class Player1():
+    def __init__(self, player, turn=False):
+        self.player = player
+        self.player_color = ''
+        self.turn = turn
+    
+    def update_player(self, game):
+        game.player1 = self.player
+
+    def get_color(self, game):
+        self.player_color = game.player1_color
+        if self.player_color == 'White':
+            self.turn = True
+
+    
+    def possible_moves_for_pawn(self, position, game):
+        curr_let = [loc for loc in position]
+        # In the game of chess, the order of block naming is file-rank,
+        # but in two dimensional arrays, we go through rows first (rank) and then column (file)
+        curr_index = [rank.index(curr_let[1]), file.index(curr_let[0])]
+        if game.board[curr_index[0]][curr_index[1]] == ['P', 'White']:
+            
+            if curr_index[0] < len(rank) - 1:           # Checking if the piece the player is looking for is at the top of the board
+
+                if curr_index[1] == 0:                  # Corner case 1 - far left
+                    move_set_index = [curr_index[0] - 1, curr_index[1] + 1]
+                    move_set_list = [file[move_set_index[1]], rank[move_set_index[0]]]
+                    move_set_let = ''.join(move_set_list)
+                    return (f'Your possible move for pawn at position {position} is {move_set_let}')
+                
+                elif curr_index[1] == len(file) - 1:    # Corner case 2 - far right
+                    move_set_index = [curr_index[0] -1, curr_index[1] - 1]
+                    move_set_list = [file[move_set_index[1]], rank[move_set_index[0]]]
+                    move_set_let = ''.join(move_set_list)
+                    return (f'Your possible move for pawn at position {position} is {move_set_let}')
+
+                else:                                   # When the piece is not in the edge of the board
+                    move_set_index = [[curr_index[0] - 1, curr_index[1] - 1], [curr_index[0] -1, curr_index[1] + 1]]
+                    move_set_list = [[file[move_set_index[0][1]], rank[move_set_index[0][0]]], [file[move_set_index[1][1]], rank[move_set_index[1][0]]]]
+                    move_set_let = [''.join(move) for move in move_set_list]
+                    return (f'Your possible moves for pawn at position {position} are {move_set_let[0]} and {move_set_let[1]}')
+            else:
+                return (f"This pawn at position {position} cannot move forward")
+        else:
+            return ('Invalid input')
+
+        ''' Need to cover cases where there is already a piece at the location the pawn can possibly move'''
+
+    def possible_moves_for_bishop(self, position, game):
+        curr_let = [loc for loc in position]
+        curr_index = [rank.index(curr_let[1]), file.index(curr_let[0])]
+        if game.board[curr_index[0]][curr_index[1]] == ['B', 'White']:
+            move_set_list = []
+            # Case 1: Up-left
+            ptr = curr_index
+            while ptr[0] > 0 and ptr[1] > 0:
+                ptr = [ptr[0] - 1, ptr[1] - 1]
+                if game.board[ptr[0]][ptr[1]] == ' ':
+                    move_set_list.append(ptr)
+
+            # Case 2: up-right
+            ptr = curr_index
+            while ptr[0] > 0 and ptr[1] < len(file) - 1:
+                ptr = [ptr[0] - 1, ptr[1] + 1]
+                if game.board[ptr[0]][ptr[1]] == ' ':
+                    move_set_list.append(ptr)
+
+            # Case 3: down-left
+            ptr = curr_index
+            while ptr[0] < len(rank) - 1 and ptr[1] > 0:
+                ptr = [ptr[0] + 1, ptr[1] - 1]
+                if game.board[ptr[0]][ptr[1]] == ' ':
+                    move_set_list.append(ptr)
+
+            # Case 4: down-right
+            ptr = curr_index
+            while ptr[0] < len(rank) - 1 and ptr[1] < len(file) - 1:
+                ptr = [ptr[0] + 1, ptr[1] + 1]
+                if game.board[ptr[0]][ptr[1]] == ' ':
+                    move_set_list.append(ptr)
+            
+            # Visualize possible moves
+            move_map = copy.deepcopy(game)
+            for move in move_set_list:
+                move_map.board[move[0]][move[1]] = '*'
+            print(f'A map of your possible moves for bishop at position {position}:')
+            return move_map.get_board()
+
+    
+
+class Player2():
+    def __init__(self, player, turn=False):
+        self.player = player
+        self.player_color = ''
+        self.turn = turn
+
+    def update_player(self, game):
+        game.player2 = self.player
+
+    def get_color(self, game):
+        self.player_color = game.player2_color
+        if self.player_color == 'White':
+            self.turn = True
